6f96ca2fc6183b058c4e843eb7e2f3df894d8bfe
Sebastian Kruse <skruse@mpiwg-berlin.mpg.de>
2013-08-28 14:46:53 +0200

    BugFix: don't create unneeded DIV in DIV

--- js/FuzzyTimeline/FuzzyTimelineRangeBars.js
+++ js/FuzzyTimeline/FuzzyTimelineRangeBars.js
@@ -47,11 +47,7 @@ function FuzzyTimelineRangeBars(parent) {
 	this.yValMax;
 	this.displayType;
 	
-	this.rangeDiv = this.parent.gui.plotDiv;
-	this.plotDiv = document.createElement("div");
-	$(this.rangeDiv).append(this.plotDiv);
-	$(this.plotDiv).width("100%");
-	$(this.plotDiv).height("100%");
+	this.plotDiv = this.parent.gui.plotDiv;
 
 	this.spanWidth;
 	this.tickSpans;
