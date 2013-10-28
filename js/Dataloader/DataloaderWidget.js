0133685ad583a997eda8528bdb528cb446d28593
Sebastian Kruse <skruse@mpiwg-berlin.mpg.de>
2013-10-28 13:15:59 +0100

    allow custom color setting via URL param for URL-loaded datasets. syntax: colorN=RRGGBB{RRGGBB}, where RR,GG,BB are hex numbers. The first color is the selected color, the second is optional and the unselected color

--- js/Dataloader/DataloaderWidget.js
+++ js/Dataloader/DataloaderWidget.js
@@ -114,6 +114,47 @@ DataloaderWidget.prototype = {
 					datasets.push(dataSet);			
 			}
 		});
+		//Load the (optional!) dataset colors
+		$.each($.url().param(),function(paramName, paramValue){
+			if (paramName.toLowerCase().startsWith("color")){
+				//color is 1-based, index is 0-based!
+				var datasetID = parseInt(paramName.substring("color".length))-1;
+				if (datasets.length > datasetID){
+					if (typeof datasets[datasetID].color === "undefined"){
+						var color = new Object();
+						var colorsSelectedUnselected = paramValue.split(",");
+						if (colorsSelectedUnselected.length > 2)
+							return;
+						
+						var color1 = colorsSelectedUnselected[0];
+						if (color1.length != 6)
+							return;
+						
+						color.r1 = parseInt(color1.substr(0,2),16);
+						color.g1 = parseInt(color1.substr(2,2),16);
+						color.b1 = parseInt(color1.substr(4,2),16);
+						
+						//check if a unselected color is given
+						if (colorsSelectedUnselected.length == 2){
+							var color0 = colorsSelectedUnselected[1];
+							if (color0.length != 6)
+								return;
+							
+							color.r0 = parseInt(color0.substr(0,2),16);
+							color.g0 = parseInt(color0.substr(2,2),16);
+							color.b0 = parseInt(color0.substr(4,2),16);
+						} else {
+							//if not: use the selected color "halved"
+							color.r0 = Math.round(color.r1/2);
+							color.g0 = Math.round(color.g1/2);
+							color.b0 = Math.round(color.b1/2);
+						}
+						
+						datasets[datasetID].color = color;
+					}	
+				}
+			}	
+		});
 		if (datasets.length > 0)
 			dataLoaderWidget.dataLoader.distributeDatasets(datasets);
 	}
